acc254f69f262bcde23e1c6d8af5d32fa0e36161
elen <34635421+sergeevaevi@users.noreply.github.com>
2019-07-13 21:11:06 +1000

    Add files via upload

--- graphics.py
+++ graphics.py
@@ -1,37 +1,54 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+ 
 fixed_df = pd.read_csv('C:\Study\sc.csv')
 plt.figure()
-fixed_df["Год"].hist(color='gray', alpha=0.5, bins=50)
+fixed_df["Год"].plot.hist(color='gray', alpha=0.5, bins=50, title = 'Статей/Год')
 fixed_df["Год"].value_counts()
-
-df3 = fixed_df.drop("Авторы", axis=1).join(fixed_df["Авторы"].str.rsplit("., ", expand=True).stack().reset_index(drop=True, level=1).rename('Names'))
-nums = df3['Names'].reset_index()
+#отделяем авторов каждой статьи
+df_a = fixed_df.drop("Авторы", axis=1).join(fixed_df["Авторы"].str.rsplit("., ", expand=True).stack().reset_index(drop=True, level=1).rename('Статей'))
+nums = df_a['Статей'].reset_index()
 nums = nums.drop("index", axis=1)
-s_names_alp = nums.groupby("Names", sort = True).size()
+s_names = nums["Статей"].value_counts()
+#находим топ-10
+df_best_10 = s_names.iloc[:10]
+#сумму остальных
+sum_other = s_names.iloc[10:].sum()
+#полное количество
+count_all = s_names.iloc[10:].shape[0]
+b = pd.Series([sum_other/count_all], index=['other'])
+df_articles = df_best_10.add(b, fill_value=0)
+plt.pie(df_articles, labels=df_articles.index.values, autopct='%i%%')
 
-a = s_names_alp.drop_duplicates()
-s_names = nums["Names"].value_counts()
-s1 = s_names.iloc[:10]
-s2 = s_names.iloc[10:].sum()
-s3 = s_names.iloc[10:].shape[0]
-b = pd.Series([s2/s3], index=['other'])
-s1 = s1.add(b, fill_value=0)
-plt.pie(s1, labels=s1.index.values, autopct='%i%%')
+fixed_df["Цитирования"].plot.hist(color='gray', alpha=0.5, bins=50, title = 'Цитируемость статей ДВФУ')
+#df4 = fixed_df.groupby("Цитирования")
+sum_quotes = fixed_df["Цитирования"].sum()
+count_quotes = fixed_df.shape[0] 
+imFac_all = sum_quotes/count_quotes
+imFac_all
+df_after_2018 = fixed_df.loc[fixed_df["Год"] >= 2018]
+#df_after_2018_grouped = df_after_2018.groupby("Цитирования")
+sum_quotes = df_after_2018["Цитирования"].sum()
+count_quotes = df_after_2018.shape[0] 
+imFac_after_2018 = sum_quotes/count_quotes
+imFac_after_2018
 
-fixed_df["Цитирования"].hist(color='k', alpha=0.5, bins=50)
-df4 = fixed_df.groupby("Цитирования")
-qo = fixed_df["Цитирования"].sum()
-val = fixed_df.shape[0] 
-imFac = qo/val
+df_best_quoted  = fixed_df.loc[fixed_df["Цитирования"] >= imFac_all] 
+df_best_quoted = df_best_quoted.sort_values(by = ['Цитирования'], ascending = [False])
+#df_best_quoted['Цитирования'].plot.hist()
 
-df5 = fixed_df.loc[fixed_df["Год"] >= 2018]
-df6 = df5.groupby("Цитирования")
-qo = df5["Цитирования"].sum()
-val = df5.shape[0] 
-imFac2 = qo/val
+df_other_quoted = df_best_quoted.iloc[:10][['Цитирования','Авторы']]
+sum_quotes = df_best_quoted.iloc[10:]['Цитирования'].sum()
+count_quotes = df_best_quoted.iloc[10:]['Цитирования'].shape[0]
+#d = {"Цитирования":pd.Series([sum_quotes/count_quotes], index=[1]), "Авторы": pd.Series(['other'], index=[1])}
+#b = pd.DataFrame(d)
+#добавим значение
+df_other_quoted.loc[10] = [sum_quotes/count_quotes, 'other']
+#s1_ = s1_.add(b)
+df_other_quoted = df_other_quoted.drop("Авторы", axis=1).join(df_other_quoted["Авторы"].str.rsplit("., ", expand=True).stack().reset_index(drop=True, level=1).rename('Авторы'))
+df_other_quoted = df_other_quoted.drop_duplicates(["Цитирования"])
 
-t2  = fixed_df.loc[fixed_df["Цитирования"] >= imFac] 
-t2 = t2.sort_values(by = ['Цитирования'], ascending = [False])
-t2['Цитирования'].hist()
+a = df_other_quoted['Цитирования'].groupby(df_other_quoted['Авторы']).sum()
+a.plot.pie(y='Цитирования', title = 'Цитирования')
+
+
